5c958c93b0cd9601624659c4cdf782534c8067e8
TaraT1 <tarathierry@gmail.com>
2026-01-13 14:20:20 -0500

    Add functionality for # of guessed letter, class - working; killing off languages, etc. - working.

--- App.jsx
+++ App.jsx
@@ -1,36 +1,49 @@
 import React, {useState} from "react"
+import {clsx} from "clsx"
 import { languages } from "./languages"
 
 export default function AssemblyEndgame() {
-    const [currentWord, setCurrentWord] = React.useState("react")
-
+    //state values
+    const [currentWord, setCurrentWord] = React.useState("react".toUpperCase())
     const [letterGuess, setLetterGuess] = React.useState([])
 
-    const languageElements = languages.map(language => {
+    //derived values
+    //incorrect guesses
+    const wrongGuessCount = letterGuess.filter(letter => !currentWord.includes(letter)).length
+
+    //Static values
+    //For language badges
+    const languageElements = languages.map((language, index) => {
+        const isLanguageLost = index < wrongGuessCount
         const styles = {
             backgroundColor: language.backgroundColor,
             color: language.color
         }
         return (
             <span 
-                className="language-badge" 
+                className={`badge ${isLanguageLost ? "lost" : ""}`}
                 key={language.name} 
                 style={styles}
                 > {language.name}
             </span>
         )
     })
+
+    //For word to be guessed
     const letterElements = currentWord.split('').map((letter, index) => (
-        <span key={index}
-            > {letter.toUpperCase()}
+        <span 
+            key={index}
+            > 
+            {letterGuess.includes(letter) ?  letter.toUpperCase() : ""}
         </span>)
     )
 
+    //Keyboard 
     const keyboardKeys = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('')
 
     // Guessed letters
     function addLetterGuess(letter) {
-        // setLetterGuess(prevLetter => [...prevLetter, letter])//allows for duplicate letters
+        // setLetterGuess(prevLetter => [...prevLetter, letter])//allows for duplicate letters; Can convert to Set, but forces re-render
         setLetterGuess(prevLetter => 
             prevLetter.includes(letter) ? 
             prevLetter : 
@@ -38,12 +51,26 @@ export default function AssemblyEndgame() {
         )
     }
 
-    const keyboardElements = keyboardKeys.map((letter) => (
-        <button 
-            key={letter} onClick={() => addLetterGuess(letter)}>
-                {letter}
+    //Update keyboard for incorrect or correct guesses
+    const keyboardElements = keyboardKeys.map(letter => {
+        const isGuess = letterGuess.includes(letter)
+        const isInWord = isGuess && currentWord.includes(letter) //background color: green
+        const isNotInWord = isGuess && !currentWord.includes(letter) //background color: red
+        const className = clsx ({
+            correct: isInWord,
+            wrong: isNotInWord
+        })
+
+        return (
+            <button 
+                className={className}
+                key={letter} 
+                onClick={() => addLetterGuess(letter)}
+            > 
+                {letter.toUpperCase()}
             </button>
-    ))
+        )
+    })
 
     return (
         <main>
